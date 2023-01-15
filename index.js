13f2158ab85a547967100afe3d7c192d8d4e33ab
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-01-15 15:08:48 -0400

    :recycle: :fire: remover delele

--- index.js
+++ index.js
@@ -269,42 +269,6 @@ app.delete("/users/:id", (req, res) => {
     });
 });
 
-
-
-
-
-
-
-
-
-// POST PROJECTS ==========================
-app.delete("/books/:id", (req, res) => {
-    const bookId = req.params.id;
-    const q = " DELETE FROM books WHERE id = ? ";
-
-    db.query(q, [bookId], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
-});
-
-app.put("/books/:id", (req, res) => {
-    const bookId = req.params.id;
-    const q = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
-
-    const values = [
-        req.body.title,
-        req.body.desc,
-        req.body.price,
-        req.body.cover,
-    ];
-
-    db.query(q, [...values, bookId], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
-});
-
 app.listen(8800, () => {
     console.log("Connected to backend.");
 });
